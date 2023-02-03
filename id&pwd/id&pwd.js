535c6130ac63cc9bd8e7f690347097c152fe7ccb
DEVELOPER_TONTO <hellokywoo11@gmail.com>
2023-02-04 00:11:29 +0900

    아이디와 비밀번호 창 빌드

---
+++ id&pwd/id&pwd.js
@@ -0,0 +1,13 @@
+function btn1(){
+    document.getElementsByClassName("ID-PWDTEXT")[0].innerHTML = "ID"
+    document.getElementsByClassName("id-pwd")[0].style.display = "block"
+}
+
+function btn2(){
+    document.getElementsByClassName("ID-PWDTEXT")[0].innerHTML = "PASSWORD"
+    document.getElementsByClassName("id-pwd")[0].style.display = "block"
+}
+
+function closing(){
+    document.getElementsByClassName("id-pwd")[0].style.display = "none"
+}
